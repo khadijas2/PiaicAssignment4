331662995f78759aaa99e68fac1d1ea9b7540dfe
Khadija Sikander <khadijasikander02@gmail.com>
2023-08-04 00:08:50 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+// //Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+let quOte = '"Do not feel lonely, the entire universe is inside you."';
+let Ans = "Maulana Rumi once said,";
+console.log(Ans + ':' + quOte);
